17e4cdf736e7476faebcbb08fb244c9c15f927a3
Dipankar Basak <143477069+Bassakdip@users.noreply.github.com>
2024-02-04 10:03:12 +0530

    Update ExpenseItem.js

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 function ExpenseItem(props){
+    const clickHandler = ()=>{
+        console.log('click!');
+    };
     return (
         <div className='expense-item'>
             <div>{props.date.toISOString()}</div>
             <div className='expense-item_des'>
                 <h2>{props.tittle}</h2>
                 <div className='expense-item_price'>{props.amount}</div>
+                <button onClick={clickHandler}>Delete expense</button>
             </div>
         </div>
     )
